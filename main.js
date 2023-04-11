247c6af3a6e571848e24d489feccfa7f0c552c54
FE-ZSJ <sjfly0611@126.com>
2023-04-11 18:40:40 +0800

    [feat]:css-loader及style-loader处理css资源

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import count from "./src/js/count";
 import sum from "./src/js/sum";
+import './src/css/index.css'
 
 console.log(count(1, 2))
 console.log(sum(1, 2, 3, 4, 5))
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-// webpack配置文件
+// webpack配置文件，打包时识别到可简化命令行操作
 const path = require('path')
 module.exports = {
     entry: './main.js', // 相对路径
@@ -8,7 +8,15 @@ module.exports = {
     },
     // loader规则匹配
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',// 将js中的css通过动态创建style标签添加到html的head生效
+                    'css-loader' // 将css资源打包成commonjs模块到js中
+                ]
+            }
+        ]
     },
     // 插件
     plugins: [],
