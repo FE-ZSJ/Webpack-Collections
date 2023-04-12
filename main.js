468c0de11c132eac9c8b393575885f6376e3593d
FE-ZSJ <sjfly0611@126.com>
2023-04-12 19:28:29 +0800

    [feat]:处理各种css预处理器的loader配置

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import count from "./src/js/count";
 import sum from "./src/js/sum";
 import './src/css/index.css'
+import './src/css/index.less'
+import './src/css/index.scss'
+import './src/css/index.styl'
 
 console.log(count(1, 2))
 console.log(sum(1, 2, 3, 4, 5))
--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,34 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',// 将js中的css通过动态创建style标签添加到html的head生效
+                    'style-loader', // 将js中的css通过动态创建style标签添加到html的head生效
                     'css-loader' // 将css资源打包成commonjs模块到js中
                 ]
-            }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    // compiles Less to CSS
+                    'style-loader',
+                    'css-loader',
+                    'less-loader' // 将less文件编译成css文件
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    // 将 JS 字符串生成为 style 节点
+                    'style-loader',
+                    // 将 CSS 转化成 CommonJS 模块
+                    'css-loader',
+                    // 将 Sass 编译成 CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.styl$/,
+                loader: "stylus-loader", // 将 Stylus 文件编译为 CSS
+            },
         ]
     },
     // 插件
