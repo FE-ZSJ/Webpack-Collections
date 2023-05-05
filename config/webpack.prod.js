3a96554acc62a807433eff8f7250e9c3dfab2679
FE-ZSJ <sjfly0611@126.com>
2023-05-05 17:51:01 +0800

    [feat]:Image Minimizer对本地静态图片无损压缩

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,6 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { extendDefaultPlugins } = require("svgo");
 const os = require('os')
 const threads = os.cpus.length
 
@@ -129,7 +131,37 @@ module.exports = {
             new CssMinimizerPlugin(), // 生产模式默认开启html、js压缩
             new TerserPlugin({ // 对js进行多进程压缩
                 parallel: threads
-            })
+            }),
+            // 压缩图片
+            // new ImageMinimizerPlugin({
+            //     minimizerOptions: {
+            //         // Lossless optimization with custom option
+            //         // Feel free to experiment with options for better result for you
+            //         plugins: [
+            //             ["gifsicle", { interlaced: true }],
+            //             ["jpegtran", { progressive: true }],
+            //             ["optipng", { optimizationLevel: 5 }],
+            //             // Svgo configuration here https://github.com/svg/svgo#configuration
+            //             [
+            //                 "svgo",
+            //                 {
+            //                     plugins: extendDefaultPlugins([
+            //                         {
+            //                             name: "removeViewBox",
+            //                             active: false,
+            //                         },
+            //                         {
+            //                             name: "addAttributesToSVGElement",
+            //                             params: {
+            //                                 attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+            //                             },
+            //                         },
+            //                     ]),
+            //                 },
+            //             ],
+            //         ],
+            //     },
+            // }),
         ]
     },
     mode: 'production',
